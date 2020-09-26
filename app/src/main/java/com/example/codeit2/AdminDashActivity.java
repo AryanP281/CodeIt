573cebcffc4194629e3b89ccd50f810b1467591c
aryan <aryanpathare281@gmail.com>
2020-09-26 23:00:44 +0530

    Auth

---
+++ app/src/main/java/com/example/codeit2/AdminDashActivity.java
@@ -0,0 +1,14 @@
+package com.example.codeit2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminDashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dash);
+    }
+}
